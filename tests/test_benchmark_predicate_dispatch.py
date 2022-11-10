c073ee31e2db7b6551cb4c3f295813a1e2a8fe18
Dmitriy Pertsev <davaeron@gmail.com>
2022-11-10 23:23:06 +0300

    upd

--- tests/test_benchmark_predicate_dispatch.py
+++ tests/test_benchmark_predicate_dispatch.py
@@ -15,12 +15,13 @@
 # type: ignore
 # pylint: skip-file
 
-from predicate_dispatch import predicate
+from predicate_dispatch import predicate_cache_result
 from multipledispatch import dispatch
 
 import pytest
 
 
+# multipledispatch
 
 @dispatch(int)
 def isint(x):
@@ -42,33 +43,92 @@ def test_benchmark_call_single_dispatch(benchmark, val):
     benchmark(isint, val)
 
 
-@pytest.mark.parametrize("val", [(1, 4)])
-def test_benchmark_call_multiple_dispatch(benchmark, val):
-    benchmark(isint, *val)
+# @pytest.mark.parametrize("val", [(1, 4)])
+# def test_benchmark_call_multiple_dispatch(benchmark, val):
+#     benchmark(isint, *val)
 
 
-def test_benchmark_add_and_use_instance(benchmark):
-    namespace = {}
+# def test_benchmark_add_and_use_instance(benchmark):
+#     namespace = {}
 
-    @benchmark
-    def inner():
-        @dispatch(int, int, namespace=namespace)
-        def mul(x, y):
-            return x * y
+#     @benchmark
+#     def inner():
+#         @dispatch(int, int, namespace=namespace)
+#         def mul(x, y):
+#             return x * y
 
-        @dispatch(str, int, namespace=namespace)
-        def mul(x, y):
-            return x * y
+#         @dispatch(str, int, namespace=namespace)
+#         def mul(x, y):
+#             return x * y
 
-        @dispatch(int, int, [float], namespace=namespace)
-        def mul(x, y, *args):
-            return x * y
+        # @dispatch(int, int, [float], namespace=namespace)
+        # def mul(x, y, *args):
+        #     return x * y
 
-        @dispatch([int], namespace=namespace)
-        def mul(*args):
-            return sum(args)
+        # @dispatch([int], namespace=namespace)
+        # def mul(*args):
+        #     return sum(args)
 
-        mul(4, 5)
-        mul('x', 5)
-        mul(1, 2, 3., 4., 5.)
-        mul(1, 2, 3, 4, 5)
+        # mul(4, 5)
+        # mul('x', 5)
+        # mul(1, 2, 3., 4., 5.)
+        # mul(1, 2, 3, 4, 5)
+
+# /multipledispatch
+
+
+# predicate_dispatch
+
+@predicate_cache_result(lambda x: x is int)
+def isint2(x):
+    return True
+
+@predicate_cache_result(lambda x: x is object)
+def isint2(x):
+    return False
+
+@predicate_cache_result()
+def isint2(x):
+    return False
+
+@pytest.mark.parametrize("val", [1, 'a'])
+def test_benchmark_call_single_dispatch2(benchmark, val):
+    benchmark(isint2, val)
+
+
+# @pytest.mark.parametrize("val", [(1, 4)])
+# def test_benchmark_call_multiple_dispatch2(benchmark, val):
+#     benchmark(isint, *val)
+
+
+# def test_benchmark_add_and_use_instance2(benchmark):
+#     namespace = {}
+
+#     @benchmark
+#     def inner():
+#         @predicate_cache_result(lambda x, y: (x is int) and (y is int))
+#         def mul(x, y):
+#             return x * y
+
+#         @predicate_cache_result(lambda x, y: (x is str) and (y is int))
+#         def mul(x, y):
+#             return x * y
+
+#         @predicate_cache_result()
+#         def mul(x, y):
+#             return x * y
+
+#         # @predicate_cache_result(lambda x, y, z: (x is int) and (y is int) and (z is list))
+#         # def mul(x, y, *args):
+#         #     return x * y
+
+#         # @predicate_cache_result(lambda x: x is list)
+#         # def mul(*args):
+#         #     return sum(args)
+
+#         mul(4, 5)
+#         mul('x', 5)
+#         # mul(1, 2, 3., 4., 5.)
+#         # mul(1, 2, 3, 4, 5)
+
+# # /predicate_dispatch
